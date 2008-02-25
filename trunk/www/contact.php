ef3620c98d7ca64d492e41e7b1adf910d7347196
Matthew Eagar <meagar@gmail.com>
2008-02-25 04:23:00 +0000

    Styling to the contact page
    git-svn-id: file:///home/meagar/subversion/westernjugglers.com@18 33bc4b81-4b0c-4cfb-a987-5301eaeb7984

--- trunk/www/contact.php
+++ trunk/www/contact.php
@@ -1,20 +1,28 @@
 <? function yield_body() { ?>
 
+<div class="Contact">
 <h2>Contact</h2>
 
 <p>The Club can be contacted in a number of ways:</p>
 
-<h3>Email</h3>
-<p><a href="mailto:uwojugglingclub@hotmail.com">uwojugglingclub@hotmail.com</a></p>
-<br/>
+<ul>
+<li>
+	<h3>Email</h3>
+	<p><a href="mailto:uwojugglingclub@hotmail.com">uwojugglingclub@hotmail.com</a></p>
+</li>
 
-<h3>Facebook</h3>
-<p>Feel free to join the <a href="http://www.facebook.com/group.php?gid=2536821735">Western Juggling Club</a> group</p>
-<br/>
+<li>
+	<h3>Facebook</h3>
+	<p>Feel free to join the <a href="http://www.facebook.com/group.php?gid=2536821735">Western Juggling Club</a> group</p>
+</li>
 
-<h3>In Person</h3>
-<p>By coming to one of our <a href="meetings.php">meetings</a>.</p>
-<br/>
+<li>
+	<h3>In Person</h3>
+	<p>By coming to one of our <a href="meetings.php">meetings</a>.</p>
+</li>
 
-<p>The site administrator can be reached at <a href="mailto:meagar@gmail.com">meagar@gmail.com</a>
+</ul>
+	<p>The site administrator can be reached at <a href="mailto:meagar@gmail.com">meagar@gmail.com</a>
+</div>
+
 <? } include('parts/layout.php') ?>
