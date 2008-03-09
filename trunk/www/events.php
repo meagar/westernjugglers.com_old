5d29c60c1718a33b3a9dbb35f7bcc8b5e09bae03
Matthew Eagar <meagar@gmail.com>
2008-03-09 17:05:12 +0000

    Indenting
    git-svn-id: file:///home/meagar/subversion/westernjugglers.com@22 33bc4b81-4b0c-4cfb-a987-5301eaeb7984

--- trunk/www/events.php
+++ trunk/www/events.php
@@ -4,12 +4,11 @@
 
 <h2>Events</h2>
 <p>
-Juggling festivals are a great place to be inspired and astounded by
-truly great juggling.  There are usually one or more vendors of
-juggling equipment present, and crash-space can usually be arranged for
-multi-day festivals.
+	Juggling festivals are a great place to be inspired and astounded by truly
+	great juggling.  There are usually one or more vendors of juggling equipment
+	present, and crash-space can usually be arranged for multi-day festivals.
 
-Events are usually added to our <?= GOOGLE_CAL_LINK ?>.
+	Events are usually added to our <?= GOOGLE_CAL_LINK ?>.
 </p>
 
 <p>Some upcoming events and festivals within driving distance of London:</p>
