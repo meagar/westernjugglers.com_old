362b480dd3153a72e4c5c2e99f75a13041c51466
Matthew Eagar <meagar@gmail.com>
2008-12-01 08:12:02 +0000

    Removed Big Red Jugglefest, added Turbo418
    git-svn-id: file:///home/meagar/subversion/westernjugglers.com@58 33bc4b81-4b0c-4cfb-a987-5301eaeb7984

--- trunk/www/_data/events.php
+++ trunk/www/_data/events.php
@@ -3,11 +3,11 @@
 return  array 
 	# new Event(name, date, location, url, notes)
 	( new Event
-		( 'Big Red Juggle Fest'
-		, 'October 24-26, 2008'
-		, 'Ithaca, New York'
-		, 'http://rso.cornell.edu/cjc/festival/'
-		, ''
+		( 'TURBO418'
+		, 'January 9 - 11, 2009'
+		, 'l\'Ecole de cirque de Quebec, 750 2nd avenue, Quebec'
+		, 'http://turbo418.com/Acceuil%20ANG.htm'
+		, 'Turbo418 is a 3 day festival at the Quebec Circus school.  This year\'s special guest will be Thomas Dietz, and you do <em>not</em> want to miss it.  There will be at least one car travelling from London with seats available, contact Matt or Sean to reserve a spot.  Absolutly worth the 10 to 11 hour drive.  Sleeping space is provided, tickets are $35 for the entire weekend (if you purchase online before Jan 8th) or $40 at the door.  Don\'t miss this festival!'
 		)
 	);
 
