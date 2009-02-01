0b594d4b5969c39bc47137a279b666d5df38ee3c
Matthew Eagar <meagar@gmail.com>
2009-02-01 00:40:34 +0000

    Remove Turbo 418, added Montreal fest
    git-svn-id: file:///home/meagar/subversion/westernjugglers.com@61 33bc4b81-4b0c-4cfb-a987-5301eaeb7984

--- trunk/www/_data/events.php
+++ trunk/www/_data/events.php
@@ -3,19 +3,26 @@
 return  array 
 	# new Event(name, date, location, url, notes)
 	( new Event
+		( 'Montreal Juggling Festival'
+		, 'May 1-3, 2009'
+		, 'Ecole Lucien Pagé, Montréal, Québec, ada'
+		, 'http://jaq.qc.ca/English/index_en.asp'
+		, 'A really amazing festival, not quite as far as the Quebec City festival.  There will be breakfast pizza.'
+		)
+		,	new Event
 		( 'RIT Spring Juggle-In'
 		, 'April 17-19 2009'
 		, 'RIT Clark Gym (Building 3), Rochester Institute of Technology, Rochester NY'
 		, 'http://www.rit.edu/sg/jugglingclub/jugglein/'
 		, 'An amazing annual festival within a relatively  easy 4 hour drive of London.  Matt and Sean will both attend, and there will be car space for those interested in coming with.'
 		)
-	 , 	new Event
+	 /*, 	new Event
 		( 'TURBO418'
 		, 'January 9 - 11, 2009'
 		, 'l\'Ecole de cirque de Quebec, 750 2nd avenue, Quebec'
 		, 'http://turbo418.com/Acceuil%20ANG.htm'
 		, 'Turbo418 is a 3 day festival at the Quebec Circus school.  This year\'s special guest will be Thomas Dietz, and you do <em>not</em> want to miss it.  There will be at least one car travelling from London with seats available, contact Matt or Sean to reserve a spot.  Absolutly worth the 10 to 11 hour drive.  Sleeping space is provided, tickets are $35 for the entire weekend (if you purchase online before Jan 8th) or $40 at the door.  Don\'t miss this festival!'
-		)
+	)*/
 	);
 
 }?>
