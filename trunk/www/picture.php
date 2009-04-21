fc995c10f873d107aeb8d5ea7c24e21e54d09453
Matthew Eagar <meagar@gmail.com>
2009-04-21 17:10:53 +0000

    Added picture.php
    git-svn-id: file:///home/meagar/subversion/westernjugglers.com@67 33bc4b81-4b0c-4cfb-a987-5301eaeb7984

---
+++ trunk/www/picture.php
@@ -0,0 +1,11 @@
+<? $picture = $_SERVER['QUERY_STRING']; ?>
+<html>
+<head>
+	<title>Western Juggling Club</title>
+</head>
+
+<body>
+	<img src="<?= $picture ?>" onclick="window.close()"/>
+</body>
+
+</html>
