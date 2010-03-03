02225778d6e5305a15b5d2f6f553c00115a6f025
Matthew Eagar <meagar@gmail.com>
2010-03-03 16:10:03 +0000

    Added WJF/IJA/RIT/Waterloo festivals for 2010
    git-svn-id: file:///home/meagar/subversion/westernjugglers.com@81 33bc4b81-4b0c-4cfb-a987-5301eaeb7984

--- trunk/www/_data/events.php
+++ trunk/www/_data/events.php
@@ -3,6 +3,34 @@
 return  array 
 	# new Event(name, date, location, url, notes)
 	( new Event
+		( 'Waterloo Juggling Festival'
+		, 'March 6-7 2010'
+		, 'Student Life Centre (SLC) of the University of Waterloo, Kitchener Waterloo'
+		, ''
+		, 'The UW Festival is a great local festival, an easy 1.5 hour drive from London.  Matt will be driving with room for two or three more people, but only on the trip there - he\'ll be heading on to Toronto for a few days immediately after the festival.  Sean usually drives too, talk with him to arrange car space.'
+		)
+	, new Event
+		( 'RIT Spring Juggle-in'
+		, 'April 16-18 2010'
+		, 'RIT Clark Gym (Building 3), Rochester Institute of Technology, Rochester NY'
+		, 'http://www.rit.edu/sg/jugglingclub/jugglein/'
+		, 'An amazing annual festival within a relatively easy 4 hour drive of London.  Matt and Sean will both attend, and there will be car space for those interested in coming with.'
+		)
+	, new Event
+		( 'WJF 6'
+		, 'July 20-25 2010'
+		, 'Riviera Hotel in Las Vegas, NV'
+		, 'http://thewjf.com/WJF6/wjf6_home.php'
+		, 'The WJF festival will be back-to-back with the IJA festival this year, and close enough to make attending both a possibility.  Matt and Sean currently plan to attend both festivals.'
+	)
+	, new Event
+		( 'IJA 63'
+		, 'July 26 - August 1 2010'
+		, 'Sparks, Nevada (adjacent to Reno), USA' 
+		, 'http://www.juggle.org/festival/'
+		, 'The biggest festival on the continent! Seven days long, following directly on the heels of the WJF festival.  The legendary <a href="http://www.youtube.com/watch?v=UkP-TW2RHuw">Kris Kremo</a> will be teaching a three day long workshop on 3b.'
+	)
+	/*( new Event
 		( 'University of Waterloo Juggling Festival'
 		, 'March 14-15 2009'
 		, 'Student Life Centre (SLC) of the University of Waterloo, Kitchener Waterloo'
