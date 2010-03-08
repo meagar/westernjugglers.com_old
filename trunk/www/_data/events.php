31e8e26087f3996ec6135bb4ee83cba2e1a66bc3
Matthew Eagar <meagar@gmail.com>
2010-03-08 20:58:25 +0000

    Removed old dates from events
    git-svn-id: file:///home/meagar/subversion/westernjugglers.com@84 33bc4b81-4b0c-4cfb-a987-5301eaeb7984

--- trunk/www/_data/events.php
+++ trunk/www/_data/events.php
@@ -3,61 +3,22 @@
 return  array 
 	# new Event(name, date, location, url, notes)
 	( new Event
-		( 'Waterloo Juggling Festival'
-		, 'March 6-7 2010'
-		, 'Student Life Centre (SLC) of the University of Waterloo, Kitchener Waterloo'
-		, 'http://juggling.uwaterloo.ca/festival.html'
-		, 'The UW Festival is a great local festival, an easy 1.5 hour drive from London.  Matt will be driving with room for two or three more people, but only on the trip there - he\'ll be heading on to Toronto for a few days immediately after the festival.  Sean usually drives too, talk with him to arrange car space.'
-		)
-	, new Event
 		( 'RIT Spring Juggle-in'
 		, 'April 16-18 2010'
 		, 'RIT Clark Gym (Building 3), Rochester Institute of Technology, Rochester NY'
 		, 'http://www.rit.edu/sg/jugglingclub/jugglein/'
 		, 'An amazing annual festival within a relatively easy 4 hour drive of London.  Matt and Sean will both attend, and there will be car space for those interested in coming with.'
-		)
-	, new Event
+	) , new Event
 		( 'WJF 6'
 		, 'July 21-25 2010'
 		, 'Riviera Hotel in Las Vegas, NV'
 		, 'http://thewjf.com/WJF6/wjf6_home.php'
 		, 'The WJF festival will be back-to-back with the IJA festival this year, and close enough to make attending both a possibility.  Matt and Sean currently plan to attend both festivals.'
-	)
-	, new Event
+	) , new Event
 		( 'IJA 63'
 		, 'July 26 - August 1 2010'
 		, 'Sparks, Nevada (adjacent to Reno), USA' 
 		, 'http://www.juggle.org/festival/'
 		, 'The biggest festival on the continent! Seven days long, following directly on the heels of the WJF festival.  The legendary <a href="http://www.youtube.com/watch?v=UkP-TW2RHuw">Kris Kremo</a> will be teaching a three day long workshop on 3b.'
-	)
-	/*( new Event
-		( 'University of Waterloo Juggling Festival'
-		, 'March 14-15 2009'
-		, 'Student Life Centre (SLC) of the University of Waterloo, Kitchener Waterloo'
-		, 'TBA'
-		, 'The UW Festival is a great local festival, an easy 1.5 hour drive from London.  Matt will be driving with room for two or three more people, but only on the trip there - he\'ll be heading on to Toronto for a few days immediately after the festival.  Sean usually drives too, talk with him to arrange car space.'
-		)
-	, new Event
-		( 'RIT Spring Juggle-In'
-		, 'April 17-19 2009'
-		, 'RIT Clark Gym (Building 3), Rochester Institute of Technology, Rochester NY'
-		, 'http://www.rit.edu/sg/jugglingclub/jugglein/'
-		, 'An amazing annual festival within a relatively easy 4 hour drive of London.  Matt and Sean will both attend, and there will be car space for those interested in coming with.'
-		)
-	, new Event
-		( 'Montreal Juggling Festival'
-		, 'May 1-3, 2009'
-		, 'Ecole Lucien Page, Montreal, Quebec, Canada'
-		, 'http://jaq.qc.ca/English/index_en.asp'
-		, 'A really amazing festival, not quite as far as the Quebec City festival.  There will be breakfast pizza.'
-		)
-	 /*, 	new Event
-		( 'TURBO418'
-		, 'January 9 - 11, 2009'
-		, 'l\'Ecole de cirque de Quebec, 750 2nd avenue, Quebec'
-		, 'http://turbo418.com/Acceuil%20ANG.htm'
-		, 'Turbo418 is a 3 day festival at the Quebec Circus school.  This year\'s special guest will be Thomas Dietz, and you do <em>not</em> want to miss it.  There will be at least one car travelling from London with seats available, contact Matt or Sean to reserve a spot.  Absolutly worth the 10 to 11 hour drive.  Sleeping space is provided, tickets are $35 for the entire weekend (if you purchase online before Jan 8th) or $40 at the door.  Don\'t miss this festival!'
-	)*/
-	);
-
+	));
 }?>
