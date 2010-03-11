473dd0da13563fffcefdb563b064d7435e873246
Matthew Eagar <meagar@gmail.com>
2010-03-11 21:46:13 +0000

    Added some new links to the links page, reformatted with traditional syntax
    git-svn-id: file:///home/meagar/subversion/westernjugglers.com@87 33bc4b81-4b0c-4cfb-a987-5301eaeb7984

--- trunk/www/_data/links.php
+++ trunk/www/_data/links.php
@@ -1,42 +1,50 @@
-<?  function getLinks() {
+<?	function getLinks() {
 	# new Link(label, url)
-	return array
-		( 'Organizations / Clubs / Festivals' => array
-			( new Link('International Jugglers\' Association', 'http://www.juggle.org/')
-			, new Link('Queen\'s / Kingston Juggling Club', 'http://kingstonjugglers.org/')
-			, new Link('World Juggling Federation', 'http://www.thewjf.com')
-			, new Link('Toronto Juggling Club', 'http://www.jugglebum.com/torontoclub.html')
-			, new Link('Cornell University Juggling Club', 'http://www.rso.cornell.edu/cjc')
-			, new Link('Waterloo Juggling Festival', 'http://juggling.uwaterloo.ca/festival.html')
-			, new Link('Victoria Juggling Club', 'http://www.uvss.uvic.ca/clubs/juggling/')
-			, new Link('RIT Spring Juggle - In', 'http://www.rit.edu/sg/jugglingclub/jugglein/')
-			, new Link('Quebec Juggling Association', 'http://www.jaq.qc.ca')
-			, new Link('New York City Jugglers - Carmine Street Irregulars', 'http://www.jugglenyc.com/')
-			)
-		, 'Equipment vendors / Manufacturers' => array
-			(	new Link('Higgins Brothers Juggling Equipment', 'http://www.higginsbrothers.com/')
-			, new Link('In the Spin Juggling Equipment', 'http://www.inthespinjuggling.com/')
-			, new Link('Renegade Juggling Equipment', 'http://www.renegadejuggling.com/')
-			, new Link('Brian Dube Juggling Equipment', 'http://www.dube.com/')
-			, new Link('Goudurix', 'http://www.goudurix.com/')
-			, new Link('Todd Smith Juggling Equipment', 'http://www.toddsmith.com/')
-			, new Link('Aerotech Projects Ltd. - Makers of Globalls', 'http://www.globall.com/')
-			, new Link('Serious Juggling', 'http://www.seriousjuggling.com/')
-			)
-		, 'Juggling Topic Sites / Forums / Performers' => array
-			( new Link('Diabolo.ca forum and videos', 'http://diabolo.ca/')
-			, new Link('Internet Juggling Database', 'http://www.jugglingdb.com/')
-			, new Link('Ryo Yabe\'s Webpage', 'http://www.hogonopro.com/%7Eryo/')
-			, new Link('Mad French Posse Diabolo site', 'http://www.artofdiabolo.com/')
-			, new Link('Juggling Information Service', 'http://www.juggling.org/')
-			, new Link('Anthony Gatto - World\'s Greatest Juggler', 'http://www.anthonygatto.com/')
-			//, new Link('JuggleThis.net - Lots of juggling videos', 'http://www.jugglethis.net/')
-			, new Link('Alan Plotkins Juggling Videos', 'http://www.festivalvideos.com/')
-			, new Link('Jason Garfield', 'http://www.jasongarfield.com/')
-			, new Link('Vova & Olga Galchenko', 'http://www.galchenko.com/')
+	return array(
+		'Organizations / Clubs / Festivals' => array(
+			new Link('International Jugglers\' Association', 'http://www.juggle.org/'),
+			new Link('Queen\'s / Kingston Juggling Club', 'http://kingstonjugglers.org/'),
+			new Link('World Juggling Federation', 'http://www.thewjf.com'),
+			new Link('Toronto Juggling Club', 'http://www.jugglebum.com/torontoclub.html'),
+			new Link('Cornell University Juggling Club', 'http://www.rso.cornell.edu/cjc'),
+			new Link('Waterloo Juggling Festival', 'http://juggling.uwaterloo.ca/festival.html'),
+			new Link('Victoria Juggling Club', 'http://www.uvss.uvic.ca/clubs/juggling/'),
+			new Link('RIT Spring Juggle - In', 'http://www.rit.edu/sg/jugglingclub/jugglein/'),
+			new Link('Quebec Juggling Association', 'http://www.jaq.qc.ca'),
+		 	new Link('New York City Jugglers - Carmine Street Irregulars', 'http://www.jugglenyc.com/')
+			),
+		'Equipment vendors / Manufacturers' => array(
+			new Link('Higgins Brothers Juggling Equipment', 'http://www.higginsbrothers.com/'),
+			new Link('In the Spin Juggling Equipment', 'http://www.inthespinjuggling.com/'),
+			new Link('Renegade Juggling Equipment', 'http://www.renegadejuggling.com/'),
+			new Link('Brian Dube Juggling Equipment', 'http://www.dube.com/'),
+			new Link('Goudurix', 'http://www.goudurix.com/'),
+			new Link('Todd Smith Juggling Equipment', 'http://www.toddsmith.com/'),
+			new Link('Aerotech Projects Ltd. - Makers of Globalls', 'http://www.globall.com/'),
+			new Link('Serious Juggling', 'http://www.seriousjuggling.com/')
+			),
+		'Juggling Topic Sites / Forums / Performers' => array(
+			new Link('Diabolo.ca forum and videos', 'http://diabolo.ca/'),
+			new Link('Internet Juggling Database', 'http://www.jugglingdb.com/'),
+			new Link('Ryo Yabe\'s Webpage', 'http://www.hogonopro.com/%7Eryo/'),
+			new Link('Mad French Posse Diabolo site', 'http://www.artofdiabolo.com/'),
+			new Link('Juggling Information Service', 'http://www.juggling.org/'),
+			new Link('Anthony Gatto - World\'s Greatest Juggler', 'http://www.anthonygatto.com/'),
+			new Link('Alan Plotkins Juggling Videos', 'http://www.festivalvideos.com/'),
+			new Link('Jason Garfield', 'http://www.jasongarfield.com/'),
+			new Link('Vova & Olga Galchenko', 'http://www.galchenko.com/'),
 			//, new Link('Luke Burrage', 'http://barnesy.org/mirror/lukeburrage/enter.html')
-			, new Link('Luke Burrage', 'http://www.lukeburrage.com/')
-			, new Link('Viktor Kee', 'http://www.viktorkee.com/')
-			)
+			new Link('Luke Burrage', 'http://www.lukeburrage.com/'),
+			new Link('Viktor Kee', 'http://www.viktorkee.com/'),
+			new Link('Matt Hall', 'http://scweb.esuhsd.org/programs/jugglesensei/'),
+			),
+			'Siteswap links' => array(
+			new Link('Siteswap on Wikipedia', 'http://en.wikipedia.org/wiki/Siteswap'),
+			new Link('Siteswap.org - A great collection of siteswap resources', 'http://www.siteswap.org/'),
+			new Link('Matt Hall\'s Siteswap list', 'http://scweb.esuhsd.org/programs/jugglesensei/SiteswapFun1.htm'),
+			new Link('Matt Hall\'s Siteswap list (printable)', 'http://dev.meagar.net/misc/siteswaps.pdf'),
+			new Link('Juggling Lab - A cross-platform siteswap animator/generator written in Java', 'http://jugglinglab.sourceforge.net/'),
+			),
 		);
+
 } ?>
